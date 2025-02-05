84b490de33d26dcae7944191247347eb1f62e8ca
나형진 <skgudwls@konkuk.ac.kr>
2025-02-05 19:31:55 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 11654                             :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: skgudwls <boj.kr/u/skgudwls>                +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/11654                          #+#        #+#      #+#    */
+/*   Solved: 2025/02/05 11:21:21 by skgudwls      ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    char input;
+    int output;
+    std::cin >> input;
+    output = input;
+    std::cout << output;
+    return 0;
+}
