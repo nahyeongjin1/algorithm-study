6ff774702aa262ccfae1b492342e56367dad808e
나형진 <skgudwls@konkuk.ac.kr>
2025-02-09 21:13:56 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 32384                             :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: skgudwls <boj.kr/u/skgudwls>                +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/32384                          #+#        #+#      #+#    */
+/*   Solved: 2025/02/09 21:12:04 by skgudwls      ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    short n;
+    std::cin >> n;
+    for (auto i = 0; i < n; i++)
+    {
+        std::cout << "LoveisKoreaUniversity ";
+    }
+    return 0;
+}
