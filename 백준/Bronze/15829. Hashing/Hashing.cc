978744fda3dceb222ba92e9eafdc4955050ee413
나형진 <skgudwls@konkuk.ac.kr>
2025-02-15 20:52:27 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2208 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 15829                             :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: skgudwls <boj.kr/u/skgudwls>                +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/15829                          #+#        #+#      #+#    */
+/*   Solved: 2025/02/15 18:25:10 by skgudwls      ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <math.h>
+
+int main()
+{
+    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+
+    long long int hash = 0;
+    const int R = 31, M = 1234567891;
+    std::string input;
+    std::cin >> input;
+    std::cin >> input;
+
+    for (auto i = 0; i < input.length(); i++)
+    {
+        short unique = input.at(i) - 'a' + 1;
+        hash += unique * std::pow(R, i);
+    }
+    hash %= M;
+
+    std::cout << hash;
+    return 0;
+}
