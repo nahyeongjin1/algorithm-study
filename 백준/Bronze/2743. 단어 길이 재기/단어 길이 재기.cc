e9504e9fe867b8a98f37828ec37b5ab844dd7bd8
나형진 <skgudwls@konkuk.ac.kr>
2025-02-19 21:10:56 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 2743                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: skgudwls <boj.kr/u/skgudwls>                +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/2743                           #+#        #+#      #+#    */
+/*   Solved: 2025/02/19 21:09:59 by skgudwls      ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    std::string input;
+    std::cin >> input;
+    std::cout << input.length();
+
+    return 0;
+}
