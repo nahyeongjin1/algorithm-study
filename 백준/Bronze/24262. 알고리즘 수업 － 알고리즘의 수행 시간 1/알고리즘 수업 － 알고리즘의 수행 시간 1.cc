7f464f7072b07fbe1b94189d40c579b53dc12dbd
나형진 <skgudwls@konkuk.ac.kr>
2026-01-21 17:58:31 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,29 @@
+// 24262
+#include <iostream>
+
+using namespace std;
+
+uint32_t n; // [1, 500'000]
+
+int main()
+{
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    cin >> n;
+
+    cout << 1 << '\n'
+         << 0 << '\n';
+
+    return 0;
+}
+
+/**
+ * 이게 대체 무슨 문제임?
+ *
+ * MenOfPassion 알고리즘이 주어진 함수일거잖아?
+ *
+ * n이 무슨 값이 들어와도 i 구하고 A[i] 반환하는건 변함이 없는거잖아?
+ *
+ * ?
+ * 뭔 개소리지 이게
+ */
